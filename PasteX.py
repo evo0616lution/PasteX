42144a3493194fff38e6cf358baa33f3055ae075
evo <84800113+evo0616lution@users.noreply.github.com>
2021-10-20 18:45:08 +0200

    Add files via upload

---
+++ PasteX.py
@@ -0,0 +1,79 @@
+import time
+import colorama
+from colorama import Fore
+import webbrowser
+import json
+import requests
+import os
+from colorama import init
+init()
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+animation = [
+"[        ]",
+"[=       ]",
+"[===     ]",
+"[====    ]",
+"[=====   ]",
+"[======  ]",
+"[======= ]",
+"[========]",
+"[ =======]",
+"[  ======]",
+"[   =====]",
+"[    ====]",
+"[     ===]",
+"[      ==]",
+"[       =]",
+"[        ]",
+"[        ]"
+]
+
+notcomplete = True
+
+i = 0
+def logo():
+  print(Fore.YELLOW + """
+
+██████╗  █████╗ ███████╗████████╗███████╗    ██╗  ██╗
+██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔════╝    ╚██╗██╔╝
+██████╔╝███████║███████╗   ██║   █████╗       ╚███╔╝ 
+██╔═══╝ ██╔══██║╚════██║   ██║   ██╔══╝       ██╔██╗ 
+██║     ██║  ██║███████║   ██║   ███████╗    ██╔╝ ██╗
+╚═╝     ╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝    ╚═╝  ╚═╝
+                                                     
+""")
+time.sleep(1)
+re = requests.get('http://evosworld.glitch.me/px_updates.html')
+if 'Update' in re.text:
+  input(Fore.GREEN + "Update avalible!")
+  webbrowser.open_new(url="http://evosworld.glitch.me/px_updates.html")
+logo()
+obj = input(Fore.RESET + "Paste text you want to upload:\n")
+time.sleep(1)
+cls()
+logo()
+print(Fore.LIGHTBLUE_EX + "Uploading...")
+while notcomplete:
+    print(animation[i % len(animation)], end='\r')
+    time.sleep(.1)
+    i += 1
+    if i == 3*10:
+      break
+cls()
+paste = "https://paste.agalts.xyz/upload?paste=" + str(obj)
+try:
+  r = requests.get(paste)
+except:
+  print(Fore.RED + "Upload failed. Check your internet connection and try again.")
+x = r.text
+y = json.loads(x)
+logo()
+print(Fore.GREEN + "Upload successful")
+print(Fore.RESET + "\n")
+print(y["url"])
+pasteurl = y["url"]
+time.sleep(0.4)
+webbrowser.open_new(url=pasteurl)
+print(Fore.CYAN + "\n")
+input("Press enter to close the program.")
